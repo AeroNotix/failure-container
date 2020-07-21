d3f156ac903b4944c1df24edd84960aa9d505efa
Aaron France <aaron.l.france@gmail.com>
2020-07-21 15:09:06 +0200

    fix: update oomkill

--- oomkill/main.go
+++ oomkill/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 )
 
 func main() {
 	x := []int{}
 	for {
+		fmt.Println("don't be hatin', just allocatin'")
 		x = append(x, rand.Intn(100))
 	}
 }
